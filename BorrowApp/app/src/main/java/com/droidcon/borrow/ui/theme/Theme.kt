5f1987078bd6f8e107559957bab83140786794c0
Daniel Bertoldi <dsdanielbertoldi@gmail.com>
2024-05-29 14:27:50 -0300

    :lipstick: Define DarkColors

--- BorrowApp/app/src/main/java/com/droidcon/borrow/ui/theme/Theme.kt
+++ BorrowApp/app/src/main/java/com/droidcon/borrow/ui/theme/Theme.kt
@@ -1,14 +1,35 @@
 package com.droidcon.borrow.ui.theme
 
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val LightColors = lightColors()
+private val LightColors = lightColors(
+  primary = md_theme_light_primary,
+  onPrimary = md_theme_light_onPrimary,
+  secondary = md_theme_light_secondary,
+  onSecondary = md_theme_light_onSecondary,
+  background = md_theme_light_background,
+  onBackground = md_theme_light_onBackground,
+  surface = md_theme_light_surface,
+  onSurface = md_theme_light_onSurface,
+)
+
+private val DarkColors = darkColors(
+  primary = md_theme_dark_primary,
+  onPrimary = md_theme_dark_onPrimary,
+  secondary = md_theme_dark_secondary,
+  onSecondary = md_theme_dark_onSecondary,
+  background = md_theme_dark_background,
+  onBackground = md_theme_dark_onBackground,
+  surface = md_theme_dark_surface,
+  onSurface = md_theme_dark_onSurface,
+)
 
 @Composable
 fun BorrowTheme(content: @Composable () -> Unit) {
-  val colors = LightColors
+  val colors = DarkColors
 
   MaterialTheme(
     colors = colors,
